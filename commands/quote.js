9cbe0969bec0338d0412009077f84f78a0009d75
Anthony Kong <anthony.kong@sfgov.org>
2020-07-21 04:06:55 -0700

    reply in thread and broadcast for quote

--- commands/quote.js
+++ commands/quote.js
@@ -5,9 +5,10 @@ const slackServices = require('../services/slack');
 const getQuote =  (payload) => {
   axios.get('http://ron-swanson-quotes.herokuapp.com/v2/quotes').then((response) => {
     let quote = response.data[0];
+    let thread_ts = payload.event.thread_ts ? payload.event.thread_ts : payload.event.ts;
     slackServices.postMessageAdvanced({
       channel: payload.event.channel,
-      thread_ts: payload.event.ts,
+      thread_ts: thread_ts,
       reply_broadcast: true,
       text: '> ' + quote
     });
