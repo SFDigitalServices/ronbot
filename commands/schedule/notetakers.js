d1f7af4433faea164e7b41eb8bd614b6d9652f25
Anthony Kong <anthony.kong@sfgov.org>
2020-07-23 22:37:59 -0700

    update commands to use db

--- commands/schedule/notetakers.js
+++ commands/schedule/notetakers.js
@@ -1,39 +1,17 @@
-const fs = require('fs');
 const path = require('path');
 const config = require('../../config.js');
 const slackServices = require('../../services/slack');
 const scheduleService = require('../../services/schedule-service.js');
-
-const appRoot = process.cwd();
-const tmpDir = path.join(appRoot + '/tmp');
-const tmpFile = '/notetakers-' + config.NOTETAKERS_SHEET.id;
 
 let scheduledNotetakerMessages = [];
 let success = '[+]';
 let fail = '[!]';
 
 const scheduleNotetakers = async (payload) => {
+  // delete previous scheduled messages to avoid duplicate messaging
+  await scheduleService.deleteScheduledMessages(config.NOTETAKERS_SHEET.id);
   // scheduled messages will also contain messages that could not be scheduled
   scheduledNotetakerMessages = await scheduleService.scheduleItems(config.NOTETAKERS_SHEET.id, config.NOTETAKERS_SHEET.range);
-  let scheduledMessagesString = JSON.stringify(scheduledNotetakerMessages);
-
-  // dump out to file to keep record of scheduled messages that may need to be deleted later
-  if(!fs.existsSync(tmpDir)) {
-    fs.mkdirSync(tmpDir);
-  }
-  fs.writeFile(tmpDir + tmpFile, scheduledMessagesString ? scheduledMessagesString : '[]', (err) => { 
-    if(err) throw err;
-    console.log('tmp file written: ' + tmpDir + '/' + tmpFile);
-  });
-
-  // also send ant a copy (to delete scheduled messages if app crashes)
-  slackServices.getUserInfo(payload.event.user).then((response) => {
-    slackServices.postMessageAdvanced({
-      "channel": "U6RS0HESK",
-      "user": "U6RS0HESK",
-      "text": '`attempted notetaker scheduling by ' + response.real_name + ' (' + response.profile.display_name + ')`' + '\n' + '```' + scheduledMessagesString + '```'
-    });
-  })
 
   try {
     let outputMessage = '```';
@@ -53,13 +31,6 @@ const scheduleNotetakers = async (payload) => {
   }
 };
 
-const refreshNotetakers = async (payload) => {
-  await scheduleService.deleteScheduledMessages(scheduledNotetakerMessages);
-  scheduledNotetakerMessages = []; // wipe out all scheduled messages because old list contains failed
-  scheduleNotetakers(payload);
-}
-
 module.exports = { 
-  scheduleNotetakers, 
-  refreshNotetakers 
+  scheduleNotetakers 
 }
