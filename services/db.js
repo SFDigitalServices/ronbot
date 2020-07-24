e536dbfe4d1e09efdc41d973d255b0e0d1cbc743
Anthony Kong <anthony.kong@sfgov.org>
2020-07-23 22:37:49 -0700

    add db service, update schedule service to use it

--- services/db.js
+++ services/db.js
@@ -1,4 +1,4 @@
-const { Client } = require('pg');
+const { Client, Pool } = require('pg');
 const config = require('../config.js');
 
 const connectionConfig = {
@@ -9,14 +9,18 @@ if(process.env.NODE_ENV === "production") {
   connectionConfig.ssl = { rejectUnauthorized: false };
 }
 
-const client = new Client(connectionConfig);
+// const client = new Client(connectionConfig);
 
-client.connect();
+// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+//   if (err) throw err;
+//   for (let row of res.rows) {
+//     // console.log(JSON.stringify(row));
+//   }
+//   client.end();
+// });
 
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
+const pool = new Pool(connectionConfig);
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+}
