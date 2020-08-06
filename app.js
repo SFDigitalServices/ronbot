5e361b53249332804cbf0f1778aedc58eb9781d6
Anthony Kong <anthony.kong@sfgov.org>
2020-08-06 00:36:54 -0700

    some debugging things

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const slack = require('./services/slack');
+
 const port = config.PORT;
 
 const slackEventsRouter = require('./routes/slack-events');
@@ -14,6 +16,27 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.get('/', (req, res) => {
   res.send('hi');
 });
+
+app.get('/test', async (req, res) => {
+  let channelList = (await slack.getRequest('https://slack.com/api/conversations.list')).channels;
+  let generalChannelId = '';
+  for(var i=0; i<channelList.length; i++) {
+    if(channelList[i].name == 'general') {
+      generalChannelId = channelList[i].id;
+      break;
+    }
+  }
+  let list = [];
+  let response = await slack.postRequest('https://slack.com/api/chat.scheduledMessages.list', {
+    channel: generalChannelId
+  });
+  // console.log(response);
+  response.scheduled_messages.forEach(item => {
+    delete item['text'];
+    list.push(item);
+  });
+  res.send(JSON.stringify(list));
+})
 
 app.use('/slack-events', slackEventsRouter);
 app.use('/slack-interactive', slacKInteractiveRouter);
