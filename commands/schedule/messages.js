04b715645846b6b64f0ff6bafef05e63b2a45d86
Anthony Kong <anthony.kong@sfgov.org>
2020-08-10 23:52:18 -0700

    update help, add some messaging around scheduling messages

--- commands/schedule/messages.js
+++ commands/schedule/messages.js
@@ -5,8 +5,12 @@ const scheduleMessages = async (payload, sheetInfo) => {
   let scheduledMessages = [];
   let success = '[+]';
   let fail = '[!]';
+  let successCount = 0;
+  let failCount = 0;
+
   // delete previous scheduled messages to avoid duplicate messaging
   await scheduleService.deleteScheduledMessages(sheetInfo.id);
+
   // scheduled messages will also contain messages that could not be scheduled
   scheduledMessages = await scheduleService.scheduleItems(sheetInfo.id, sheetInfo.range);
 
@@ -15,13 +19,21 @@ const scheduleMessages = async (payload, sheetInfo) => {
     outputMessage += success + ' = success, ' + fail + ' = fail' + "\n";
     scheduledMessages.forEach(item => {
       if(item.ok) {
+        successCount++;
         outputMessage += success;
       } else{
+        failCount++;
         outputMessage += fail;
       }
       outputMessage += ' ' + item.recipient + ' for ' + item.datetime + "\n";
     });
+    outputMessage += '```' + "\n";
     outputMessage += '```';
+    outputMessage += 'success: ' + successCount + "\n";
+    outputMessage += 'fail: ' + failCount;
+    outputMessage += '```' + "\n";
+    outputMessage += "Failures usually happen because the date and/or time is in the past or too far in the future.";
+
     slackServices.postMessage(payload, outputMessage);
   } catch(e) {
     console.log(e);
