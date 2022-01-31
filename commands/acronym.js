23dc911d72c59b5827d2fb94a86921f691b05658
Shawn Allen <shawnbot@users.noreply.github.com>
2022-01-31 13:51:24 -0800

    chore(lint): lint and refactor acronym.js

--- commands/acronym.js
+++ commands/acronym.js
@@ -1,62 +1,60 @@
-const config = require('../config.js');
-const slackServices = require('../services/slack');
-const googleSheets = require('../services/google-sheets');
+const { ACRONYMS_SHEET } = require('../config')
+const slackServices = require('../services/slack')
+const googleSheets = require('../services/google-sheets')
 
-let acronyms = {list: {}};
+let acronyms = { list: {} }
 
-const loadAcronyms = async (acronyms) => {
+async function loadAcronyms (acronyms) {
   try {
-    acronyms.list = {};
-    let items = (await googleSheets.getSheet(config.ACRONYMS_SHEET.id, config.ACRONYMS_SHEET.range)).values;
-    if(items.length > 0 ) {
-      items.forEach((element, index) => {
-        if(index > 0) {
-          if(element.length == 2) {
-            acronyms.list[element[0]] = element[1];
-          }
+    acronyms.list = {}
+    const items = (
+      await googleSheets.getSheet(ACRONYMS_SHEET.id, ACRONYMS_SHEET.range)
+        .catch(error => ({ values: [] }))
+    )?.values || []
+    items.forEach((element, index) => {
+      if (index > 0) {
+        if (element.length === 2) {
+          acronyms.list[element[0]] = element[1];
         }
-      });
-    }
-    return items;
-  } catch (err) {
-    console.log(err);
+      }
+    })
+    return items
+  } catch (error) {
+    console.error('[loadAcronyms] error:', error.message)
+    return []
   }
 };
 
-const refreshAcronyms = async () => {
+async function refreshAcronyms () {
   const currentCount = Object.keys(acronyms.list).length;
   try {
-    const items = await loadAcronyms(acronyms);
-    const itemsLength = items.length - 1; // exclude header row
-    diff = currentCount > itemsLength ? currentCount - itemsLength : itemsLength - currentCount;
-    return { status: itemsLength > 0, diff: diff };
-  } catch(err) {
-    console.log(err);
+    const items = await loadAcronyms(acronyms)
+    const itemsLength = items.length - 1 // exclude header row
+    const diff = currentCount > itemsLength
+      ? currentCount - itemsLength
+      : itemsLength - currentCount
+    return {
+      status: itemsLength > 0,
+      diff
+    }
+  } catch (err) {
+    console.log(err)
   }
 }
 
-const getAcronym = (payload, acronym) => {
-  if(acronym) {
-    let found = false;
-
-    if(Object.keys(acronyms.list).length > 0) {
-      let definition = acronyms.list[acronym]
-      if(definition) {
-        found = true;
-        let text = '*' + acronym + '* is probably an acronym for _' + definition + '_';
-        slackServices.postMessage(payload, text);
-      }
-    }
-  
-    if(!found) {
-      let text = "The acronym you requested (*" + acronym + "*) was not found.  Add it to the <https://docs.google.com/spreadsheets/d/13fcfWufGFEIVvca-1hP7mchVg9q6n7LOvdxYDBTdQiw|acronyms sheet> and then issue the `refresh acronyms` command";
-      slackServices.postMessage(payload, text);
+function getAcronym (payload, acronym) {
+  if (acronym) {
+    const definition = acronyms.list[acronym]
+    if (definition) {
+      slackServices.postMessage(payload, `*${acronym}* is probably an acronym for _${definition}_`)
+    } else {
+      slackServices.postMessage(payload, "The acronym you requested (*" + acronym + "*) was not found.  Add it to the <https://docs.google.com/spreadsheets/d/13fcfWufGFEIVvca-1hP7mchVg9q6n7LOvdxYDBTdQiw|acronyms sheet> and then issue the `refresh acronyms` command")
     } 
   } else {
     slackServices.postMessage(payload, "`No acronym requested`")
   }
-};
+}
 
-loadAcronyms(acronyms);
+loadAcronyms(acronyms)
 
 module.exports = { getAcronym, refreshAcronyms }
