1f5e5efe01b083ee2ecf8205b61bbc9877f5d245
anthony kong <anthony.kong@sfgov.org>
2022-05-21 23:32:44 -0700

    create google cred file and load acronyms after npm install

--- postinstall.js
+++ postinstall.js
@@ -1,3 +1,7 @@
+const acronyms = require('./commands/acronym')
+
 if(process.env.NODE_ENV === 'undefined' || process.env.NODE_ENV !== 'production') require('dotenv').config();
 const fs = require('fs');
 fs.writeFile(process.env.GOOGLE_APPLICATION_CREDENTIALS, process.env.GOOGLE_CONFIG, (err) => {});
+
+acronyms.load();
