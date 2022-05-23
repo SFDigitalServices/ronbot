44ed921c9ded188f132a08c65dd98111bd417b1b
anthony kong <anthony.kong@sfgov.org>
2022-05-23 09:02:22 -0700

    remove commented code

--- routes/ghost-inspector.js
+++ routes/ghost-inspector.js
@@ -4,29 +4,6 @@ const router = express.Router();
 const axios = require('axios')
 const airtable = require('../services/airtable');
 
-// router.get('/suite-results/:suiteResultId', (req, res) => {
-//   const suiteResultId = req.params.suiteResultId;
-//   airtable.getRecords('ghost_inspector', {
-//     filterByFormula: '({suite_result}="' + suiteResultId + '")',
-//     sort: [{field: 'test_name', direction: 'asc'}]
-//   }).then((result) => {
-//     if(result && result.length > 0) {
-//       res.render('suite-results', { 
-//         data: {
-//           suiteResultId: suiteResultId,
-//           environment: result[0].fields.environment,
-//           records: result
-//         }
-//       });
-//     } else {
-//       res.send('no records');
-//     }
-//   }).catch((error) => {
-//     console.error(error);
-//     res.send(error);
-//   });
-// });
-
 router.get('/suite-results/:suiteResultId', (req, res) => {
   const suiteResultId = req.params.suiteResultId
   // get the suite tests (for proper ordering)
