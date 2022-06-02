c7fe3924a1d43f1d518d5269e3f5d8eae0256497
anthony kong <anthony.kong@sfgov.org>
2022-06-02 02:24:34 -0700

    update suite results

--- routes/ghost-inspector.js
+++ routes/ghost-inspector.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const axios = require('axios')
 const airtable = require('../services/airtable');
 
-router.get('/suite-results/:suiteResultId', (req, res) => {
+router.get('/suite-results/:suiteId/:suiteResultId', (req, res) => {
   const suiteResultId = req.params.suiteResultId
   // get the suite tests (for proper ordering)
-  axios.get('https://api.ghostinspector.com/v1/suites/60abebb3ef68371384feb6d2/tests/?apiKey=' + config.GHOST_INSPECTOR_API_KEY).then((suiteTestsResult) => {
+  axios.get('https://api.ghostinspector.com/v1/suites/' + req.params.suiteId + '/tests/?apiKey=' + config.GHOST_INSPECTOR_API_KEY).then((suiteTestsResult) => {
     const suiteTests = suiteTestsResult.data.data
     const suiteTestsCount = suiteTests.length
     const results = { meta: {}, data: {} }
@@ -36,10 +36,13 @@ router.get('/suite-results/:suiteResultId', (req, res) => {
     // there is a max limit of test results in a suite run of 50, figure out the offset to get all the test results
     const numCalls = Math.ceil(suiteTestsCount/50)
     let count = 50
-    // let params = '&count=' + count + '&offset=' + offset
-    // let giUrl = 'https://api.ghostinspector.com/v1/suite-results/' + suiteResultId + '/results?apiKey=' + config.GHOST_INSPECTOR_API_KEY + params
     let promises = []
+    let baselinePromises = []
     const suiteTestResults = []
+    const baselineResults = {}
+
+    // console.log(results)
+
     for(let i=0; i<numCalls; i++) {
       let params = '&count=' + count + '&offset=' + (count * i)
       let giUrl = 'https://api.ghostinspector.com/v1/suite-results/' + suiteResultId + '/results?apiKey=' + config.GHOST_INSPECTOR_API_KEY + params
@@ -48,53 +51,83 @@ router.get('/suite-results/:suiteResultId', (req, res) => {
         axios.get(giUrl).then(response => {
           for(let j=0; j<response.data.data.length; j++) {
             suiteTestResults.push(response.data.data[j])
+            baselineResults[response.data.data[j].test._id] = { "baselineResult": response.data.data[j].screenshotCompareBaselineResult }
           }
         })
       )
     }
 
-    Promise.all(promises).then(() => {
-      for(let i=0; i<suiteTestResults.length; i++) {
-        let suiteResult = suiteTestResults[i]
-        let testId = suiteResult.test._id
-        if(i==0) { // get some meta info
-          results.meta.startUrl = suiteResult.startUrl
-        }
-        if(testId in results.data) {
-          results.data[testId].passing = suiteResult.passing
-          results.data[testId].screenshotCompareDifference = suiteResult.screenshotCompareDifference
-          results.data[testId].screenshotComparePassing = suiteResult.screenshotComparePassing
-          results.data[testId].screenshot = {
-            original: suiteResult.screenshot.original,
-            small: suiteResult.screenshot.small
-          }
-          results.data[testId].isMobile = suiteResult.viewportSize.width == 375
+    
 
-          // sort the passing/failing tests for navigation
-          if(suiteResult.screenshotComparePassing == true) {
-            delete testList.failing[testId]
-          }
-          
-          // capture screenshot comparison
-          if(suiteResult.screenshotComparePassing == false) {
-            results.data[testId].screenshotCompare = {
-              compareOriginal: suiteResult.screenshotCompare.compareOriginal,
-              compareSmall: suiteResult.screenshotCompare.compareSmall
-            }
-            // and also sort
-            delete testList.passing[testId]
-          }
-        }
+    
+
+    Promise.all(promises).then(() => {
+      for(let x=0; x<baselineResults.length; x++) {
+        baselinePromises.push(
+          axios.get('https://api.ghostinspector.com/v1/results/' + baselineResults[x].baselineResult   + '/?apiKey=' + config.GHOST_INSPECTOR_API_KEY).then(response => {
+            baselineResults[x].baselineScreenshot = response.data.data.screenshot.original.defaultUrl
+          })
+        )
       }
 
-      console.log(testList)
+      for(let key in baselineResults) {
+        baselinePromises.push(
+          axios.get('https://api.ghostinspector.com/v1/results/' + baselineResults[key].baselineResult   + '/?apiKey=' + config.GHOST_INSPECTOR_API_KEY).then(response => {
+            baselineResults[key].baselineScreenshot = response.data.data.screenshot.original.defaultUrl
+          })
+        )
+      }
 
-      res.render('suite-results', {
-        data: {
-          suiteResultId: suiteResultId,
-          testList: testList,
-          results: results
+      Promise.all(baselinePromises).then(() => {
+        for(let i=0; i<suiteTestResults.length; i++) {
+          let suiteResult = suiteTestResults[i]
+          let testId = suiteResult.test._id
+          
+          if(i==0) { // get some meta info
+            results.meta.startUrl = suiteResult.startUrl
+          }
+  
+          if(testId in results.data) {
+            results.data[testId].passing = suiteResult.passing
+            results.data[testId].screenshotCompareDifference = suiteResult.screenshotCompareDifference
+            results.data[testId].screenshotComparePassing = suiteResult.screenshotComparePassing
+            results.data[testId].isMobile = suiteResult.viewportSize.width == 375
+
+            // new screenshot
+            results.data[testId].screenshot = {
+              original: suiteResult.screenshot.original,
+              small: suiteResult.screenshot.small
+            }
+
+            // baseline screenshot
+            results.data[testId].screenshot.baselineScreenshot = baselineResults[testId].baselineScreenshot
+  
+            // sort the passing/failing tests for navigation
+            if(suiteResult.screenshotComparePassing == true) {
+              delete testList.failing[testId]
+            }
+            
+            // capture screenshot comparison
+            if(suiteResult.screenshotComparePassing == false) {
+              results.data[testId].screenshotCompare = {
+                compareOriginal: suiteResult.screenshotCompare.compareOriginal,
+                compareSmall: suiteResult.screenshotCompare.compareSmall
+              }
+              // and also sort
+              delete testList.passing[testId]
+            }
+          }
         }
+        
+        console.log(results)
+  
+        res.render('suite-results', {
+          data: {
+            suiteResultId: suiteResultId,
+            testList: testList,
+            results: results
+          }
+        })
       })
     })
   })
